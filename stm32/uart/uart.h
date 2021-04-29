abea53a8e1d09d1ac5177acaeb4e591d238338e3
BadNews13 <nitro5@bk.ru>
2021-04-29 15:01:14 +0300

    call C in C++
    внесена правка в заголовочный файл для вызова C функций в С++

--- stm32/uart/uart.h
+++ stm32/uart/uart.h
@@ -8,8 +8,11 @@
 #ifndef UART_H_
 #define UART_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "stm32f10x.h"
-//#include "main.h"
 
 void USART1_Init(void);
 void USART1_IRQHandler(void);
@@ -21,9 +24,10 @@ void DMA1_Init (void);
 char rx_str[RX_BUFFER_SIZE];
 char tx_str[TX_BUFFER_SIZE];
 
-
 void put_byte_UART1(uint8_t c);
 
-
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* UART_H_ */
